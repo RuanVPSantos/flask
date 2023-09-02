3a1567da0b841c4c27f38f5ee24e64de94122ff4
RuanVPSantos <ruanvpsantos@gmail.com>
2023-09-02 18:01:37 -0300

    first deploy flask api

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 from flask import Flask, jsonify
 import os
+from flask import Flask, send_from_directory
+from flask_restful import Api, Resource, reqparse
+from flask_cors import CORS  # comment this on deployment
+from api.HelloApiHandler import HelloApiHandler
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="", static_folder="frontend/build")
+CORS(app)  # comment this on deployment
+api = Api(app)
 
 
-@app.route('/')
+@app.route("/", defaults={"path": ""})
+def serve(path):
+    return send_from_directory(app.static_folder, "index.html")
+
+
+@app.route("/")
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    return "<h1>Hello World</h1>"
 
 
-if __name__ == '__main__':
+api.add_resource(HelloApiHandler, "/flask/hello")
+
+
+if __name__ == "__main__":
     app.run(debug=True, port=os.getenv("PORT", default=5000))
